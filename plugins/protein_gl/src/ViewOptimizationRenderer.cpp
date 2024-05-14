a4288231c07317e42819f48e26451e994575b343
Marcel Heine <M_Heine@web.de>
2024-05-14 10:14:36 +0200

    Added faulty dummy implementaion

--- plugins/protein_gl/src/ViewOptimizationRenderer.cpp
+++ plugins/protein_gl/src/ViewOptimizationRenderer.cpp
@@ -1,23 +1,41 @@
 /**
  * MegaMol
- * Copyright (c) 2024, MegaMol Dev Team
+ * Copyright (c) 2024 by Marcel Heine
  * All rights reserved.
  */
 
 #include "ViewOptimizationRenderer.h"
 
-// #include "mmcore/view/Camera.h"
+#include <glm/glm.hpp>
+
+#include "geometry_calls_gl/CallTriMeshDataGL.h"
+#include "mmcore/view/Camera.h"
 #include "mmstd_gl/renderer/CallRender3DGL.h"
+#include "mmcore/param/FloatParam.h"
 
-//#include "geometry_calls_gl/CallTriMeshDataGL.h"
-
-
+#include "protein_calls/MolecularDataCall.h"
 
 using namespace megamol;
+using namespace megamol::core::param;
+using namespace megamol::core::view;
 using namespace megamol::protein_gl;
 
-ViewOptimizationRenderer::ViewOptimizationRenderer() {
+ViewOptimizationRenderer::ViewOptimizationRenderer()
+        : getMacromoleculeMeshData_("getMacromoleculeMeshData",
+              "Connects the renderer to a data provider to retrieve macromolecule mesh data")
+        , getLigandPDBData_(
+              "getLigandPDBData", "Connects the renderer to a data provider to retrieve lignad mesh data")
+        , camXCoord_("camXCoordinate", "changes the x-coordinate of the camera positon") {
 
+    getMacromoleculeMeshData_.SetCompatibleCall<megamol::geocalls_gl::CallTriMeshDataGLDescription>();
+    getMacromoleculeMeshData_.SetNecessity(megamol::core::AbstractCallSlotPresentation::Necessity::SLOT_OPTIONAL);
+    this->MakeSlotAvailable(&getMacromoleculeMeshData_);
+
+    this->getLigandPDBData_.SetCompatibleCall<protein_calls::MolecularDataCallDescription>();
+    this->MakeSlotAvailable(&this->getLigandPDBData_);
+
+    this->camXCoord_.SetParameter(new megamol::core::param::FloatParam(0.0f));
+    this->MakeSlotAvailable(&this->camXCoord_);
 }
 
 ViewOptimizationRenderer::~ViewOptimizationRenderer() {
@@ -31,10 +49,25 @@ bool ViewOptimizationRenderer::create() {
 void ViewOptimizationRenderer::release() {}
 
 bool ViewOptimizationRenderer::Render(mmstd_gl::CallRender3DGL& call) {
+    auto& cam = call.GetCamera();
+    const auto& pos = cam.getPose();
+    auto new_pose = pos;
+
+    float x_pos = this->camXCoord_.Param<core::param::FloatParam>()->Value();
+    auto const center_ = call.AccessBoundingBoxes().BoundingBox().CalcCenter();
+
+    const glm::vec3 cam_pos = pos.position;
+    const glm::vec3 new_cam_pos = glm::vec3(x_pos, center_.GetY(), center_.GetZ());
+
+    new_pose.position = new_cam_pos;
+    const auto& cam_intrinsics = cam.get<Camera::PerspectiveParameters>();
+
+    call.SetCamera(Camera(new_pose, cam_intrinsics));
+
+
     return true;
 }
 
 bool ViewOptimizationRenderer::GetExtents(mmstd_gl::CallRender3DGL& call) {
     return true;
 }
-
--- plugins/protein_gl/src/ViewOptimizationRenderer.h
+++ plugins/protein_gl/src/ViewOptimizationRenderer.h
@@ -1,13 +1,16 @@
 /**
  * MegaMol
- * Copyright (c) 2024, MegaMol Dev Team
+ * Copyright (c) 2024 by Marcel Heine
  * All rights reserved.
  */
 
 #pragma once
 
+#include <glm/glm.hpp>
+
 #include "mmcore/CallerSlot.h"
 #include "mmcore/param/ParamSlot.h"
+#include "mmcore/view/Camera.h"
 #include "mmstd_gl/renderer/Renderer3DModuleGL.h"
 
 namespace megamol::protein_gl {
@@ -36,7 +39,10 @@ protected:
     bool Render(mmstd_gl::CallRender3DGL& call) override;
 
 private:
+    core::CallerSlot getMacromoleculeMeshData_;
+    core::CallerSlot getLigandPDBData_;
 
+    core::param::ParamSlot camXCoord_;
 
 }; // class ViewpointOptimizer
 
