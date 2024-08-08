8709a1b257df52a109fd4d0cb99ed66f537bb983
Marcel Heine (Sand) <M_Heine@web.de>
2024-08-08 17:53:28 +0200

    Minor Refactorings

--- plugins/protein_gl/src/ViewOptimizationRenderer.cpp
+++ plugins/protein_gl/src/ViewOptimizationRenderer.cpp
@@ -25,14 +25,14 @@ using namespace megamol::core::view;
 using namespace std;
 
 ViewOptimizationRenderer::ViewOptimizationRenderer()
-        : getMacromoleculeMeshData_("getMacromoleculeMeshData",
-              "Connects the renderer to a data provider to retrieve macromolecule mesh data")
+        : getTargetMeshData_("getTargetMeshData",
+              "Connects the renderer to a data provider to retrieve target mesh data")
         , getLigandPDBData_("getLigandPDBData", "Connects the renderer to a data provider to retrieve lignad mesh data")
         , optimizeCamera_("OptimizeCamera", "Acts as a button to set the camera to view the ligand binding site") {
 
-    this->getMacromoleculeMeshData_.SetCompatibleCall<megamol::geocalls_gl::CallTriMeshDataGLDescription>();
-    this->getMacromoleculeMeshData_.SetNecessity(megamol::core::AbstractCallSlotPresentation::Necessity::SLOT_REQUIRED);
-    this->MakeSlotAvailable(&getMacromoleculeMeshData_);
+    this->getTargetMeshData_.SetCompatibleCall<megamol::geocalls_gl::CallTriMeshDataGLDescription>();
+    this->getTargetMeshData_.SetNecessity(megamol::core::AbstractCallSlotPresentation::Necessity::SLOT_REQUIRED);
+    this->MakeSlotAvailable(&getTargetMeshData_);
 
     this->getLigandPDBData_.SetCompatibleCall<protein_calls::MolecularDataCallDescription>();
     this->getLigandPDBData_.SetNecessity(megamol::core::AbstractCallSlotPresentation::Necessity::SLOT_REQUIRED);
@@ -69,36 +69,33 @@ bool ViewOptimizationRenderer::Render(mmstd_gl::CallRender3DGL& call) {
         if (ligand->AtomCount() == 0)
             return true;
 
-        /* ------- Get Macromolecule Data ------- */
-        geocalls_gl::CallTriMeshDataGL* macroMol =
-            this->getMacromoleculeMeshData_.CallAs<megamol::geocalls_gl::CallTriMeshDataGL>();
-        if (macroMol == nullptr) {
+        /* ------- Get Target Data ------- */
+        geocalls_gl::CallTriMeshDataGL* target =
+            this->getTargetMeshData_.CallAs<megamol::geocalls_gl::CallTriMeshDataGL>();
+        if (target == nullptr) {
             return false;
         }
-        macroMol->SetFrameID(static_cast<int>(call.Time()));
-        if (!(*macroMol)(1)) {
+        target->SetFrameID(static_cast<int>(call.Time()));
+        if (!(*target)(1)) {
             return false;
         }
 
-        macroMol->SetFrameID(static_cast<int>(call.Time()));
-        if (!(*macroMol)(0)) {
+        target->SetFrameID(static_cast<int>(call.Time()));
+        if (!(*target)(0)) {
             return false;
         }
 
         /* ------- Make Naive Camera Calculations ------- */
 
         // get ligand ceneter coordinates
-        float* pos0 = new float[ligand->AtomCount() * 3];
+        const unsigned int ligandAtomCount = ligand->AtomCount();
+        float* pos0 = new float[ligandAtomCount * 3];
+        memcpy(pos0, ligand->AtomPositions(), ligandAtomCount * 3 * sizeof(float));
 
-        memcpy(pos0, ligand->AtomPositions(), ligand->AtomCount() * 3 * sizeof(float));
-
-        glm::vec3 ligandCenter = glm::vec3(0, 0, 0);
-        for (unsigned int i = 0; i < ligand->AtomCount() * 3; i++) {
-            ligandCenter[i % 3] += pos0[i] / ligand->AtomCount();
-        }
+        glm::vec3 ligandCenter = moleculeCenter(pos0, ligandAtomCount);
 
         // get radius, which will be used to select vertices around the ligand center
-        // i.e get max atom distance from ligand center + atom sphere radius + gap of macro molecule-ligand
+         // i.e get max atom distance from ligand center + atom sphere radius + gap of target-ligand
         float radius = 0;
         for (int i = 0; i < ligand->AtomCount(); i++) {
             float dist = glm::distance(glm::vec3(pos0[i * 3], pos0[i * 3 + 1], pos0[i * 3 + 2]), ligandCenter);
@@ -106,17 +103,17 @@ bool ViewOptimizationRenderer::Render(mmstd_gl::CallRender3DGL& call) {
                 radius = dist;
             }
         }
-        // adding value approximatly of average gap between ligand and macro molecule (arbitrarily chosen)
+        // adding value approximatly of average gap between ligand and target (arbitrarily chosen)
         // changes in this value might/should/will significantly change the camer orientation
-        const float atomRadiusAndGap = 2.0f;    
+        const float atomRadiusAndGap = 2.0f;
         radius += atomRadiusAndGap;
 
-        // get obj: the (first) object of macromolecule caller slot. Used for the vertex data
-        const auto& obj = macroMol->Objects()[0];
+        // get obj: the (first) object of target caller slot. Used for the vertex data
+        const auto& obj = target->Objects()[0];
         auto vertCount = obj.GetVertexCount();
         auto triCount = obj.GetTriCount();
 
-        // find all macro molecule tie mesh vertices that are within the 'radius' aroud the ligand center
+        // find all target tie mesh vertices that are within the 'radius' aroud the ligand center
         // std::list<unsigned int> selectedVerticesIndex;
         glm::vec3 naiveCamDirection = glm::vec3(0, 0, 0);
         unsigned int nrSelectedVertices = 0;
@@ -129,21 +126,15 @@ bool ViewOptimizationRenderer::Render(mmstd_gl::CallRender3DGL& call) {
             }
         }
         naiveCamDirection = glm::normalize(naiveCamDirection);
+        cout << "CORDS: " << naiveCamDirection[0] << "," << naiveCamDirection[1] << "," << naiveCamDirection[2];
 
         /* ------- Set New Camera ------- */
 
-        // Change the camera position coordinates of the CallRenderer3DGL call
-        auto& cam = call.GetCamera();
-        Camera::Pose newCamPose = cam.getPose();  // old camera as default
-
-        // camera-ligandcenter distance is _ times the vertex selection radius
         const float camDistFactor = 16.0f;
-        newCamPose.position = ligandCenter + naiveCamDirection * glm::vec3(radius * camDistFactor);
-        newCamPose.direction = -naiveCamDirection;
-
-        const auto& cam_intrinsics = cam.get<Camera::PerspectiveParameters>();
         
-        call.SetCamera(Camera(newCamPose, cam_intrinsics));
+        newCallCamera(call, naiveCamDirection, ligandCenter, radius, camDistFactor);
+
+        /* ------- Cleanup & Global Value(s) ------- */
 
         // delete pointers and ensure, that this if branch is executed once until user request
         this->optimizeCamera_.Param<core::param::BoolParam>()->SetValue(false);     
@@ -153,12 +144,8 @@ bool ViewOptimizationRenderer::Render(mmstd_gl::CallRender3DGL& call) {
     return true;
 }
 
-int temoFunction() {
-    return 5;
-}
-
 bool ViewOptimizationRenderer::GetExtents(mmstd_gl::CallRender3DGL& call) {
-    auto ctmd = this->getMacromoleculeMeshData_.CallAs<geocalls_gl::CallTriMeshDataGL>();
+    auto ctmd = this->getTargetMeshData_.CallAs<geocalls_gl::CallTriMeshDataGL>();
     if (ctmd == nullptr) {
         return false;
     }
@@ -173,3 +160,30 @@ bool ViewOptimizationRenderer::GetExtents(mmstd_gl::CallRender3DGL& call) {
 
     return true;
 }
+
+glm::vec3 ViewOptimizationRenderer::moleculeCenter(float* positions, unsigned int atomCount) {
+    glm::vec3 ligandCenter = glm::vec3(0, 0, 0);
+    for (unsigned int i = 0; i < atomCount * 3; i++) {
+        ligandCenter[i % 3] += positions[i] / atomCount;
+    }
+
+    return ligandCenter;
+}
+
+void ViewOptimizationRenderer::newCallCamera(mmstd_gl::CallRender3DGL& call, glm::vec3 direction, glm::vec3 ligCenter, float radius, float camDistFactor) {
+    // Change the camera position coordinates of the CallRenderer3DGL call
+    auto& cam = call.GetCamera();
+    Camera::Pose newCamPose = cam.getPose();  // old camera as default
+
+    // camera-ligandcenter distance is _ times the vertex selection radius
+    newCamPose.position = ligCenter + direction * glm::vec3(radius * camDistFactor);
+    newCamPose.direction = -direction;
+
+    const auto& camIntrinsics = cam.get<Camera::PerspectiveParameters>();
+
+    call.SetCamera(Camera(newCamPose, camIntrinsics));
+}
+
+//geocalls_gl::CallTriMeshDataGL::Mesh naiveCavetyCutter(geocalls_gl::CallTriMeshDataGL::Mesh mesh, glm::vec3 ligCenter) {
+
+//}
--- plugins/protein_gl/src/ViewOptimizationRenderer.h
+++ plugins/protein_gl/src/ViewOptimizationRenderer.h
@@ -19,11 +19,11 @@ namespace megamol::protein_gl {
 class ViewOptimizationRenderer : public mmstd_gl::Renderer3DModuleGL {
 public:
     static const char* ClassName() {
-        return "ViewpointOptimizer";
+        return "ViewOptimizationRenderer";
     }
 
     static const char* Description() {
-        return "Camera viwepoint optimizer for protein-ligand docking";
+        return "Camera viwepoint optimizer for protein-ligand docking scenes";
     }
 
     static bool IsAvailable() {
@@ -41,13 +41,37 @@ protected:
 
 private:
     /* Require the mesh data from protein and ligand*/
-    core::CallerSlot getMacromoleculeMeshData_;
+    core::CallerSlot getTargetMeshData_;
     core::CallerSlot getLigandPDBData_;
 
     /* Module parameters */
     core::param::ParamSlot optimizeCamera_;
 
-    bool isInputXPosChanged;
+    /* =========== Functions =========== */
+
+    /*
+     * Determine the center coordinate (x1,x2,x3) by averaging over all atom positions
+     * defaults to (0,0,0) if no atoms are contained within the call
+     *
+     * Input: float array of length = 0 (mod 3), where sets of three denote the (x,y,z) coordinates of an atom
+     */
+    glm::vec3 moleculeCenter(float* coordinates, unsigned int atomCount);
+
+    /*
+     * Calculate a camera direction vector in a naive manner
+     * Summs up the vectors from within range target mesh vertices to the ligand center and normalizes the result
+     */
+    //glm::vec3 naiveCameraDirection(megamol::geocalls_gl::CallTriMeshDataGL::Mesh obj, const glm::vec3 center, const float radius);
+
+    /*
+     * Replace the camera position and direction of a call with new values 
+     */
+    void newCallCamera(megamol::mmstd_gl::CallRender3DGL& call, glm::vec3 direction, glm::vec3 ligCenter, float radius, float camDistFactor);
+
+    /* remove all mesh vertices, that do not lay within a certain radius around a center coordinate */
+    //megamol::geocalls_gl::CallTriMeshDataGL::Mesh naiveCavetyCutter(megamol::geocalls_gl::CallTriMeshDataGL::Mesh, glm::vec3, float);
+
+
 
 }; // class Vie//wpointOptimizer
 
